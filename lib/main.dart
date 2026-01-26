e7c2d547f707c426c43a3a4df409cd2ab9371d7e
EnguerranCA <enguerran.caroalquier@etu.unilim.fr>
2026-01-26 16:30:58 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'movie_list_page.dart';
+import 'services/movie_service.dart';
+
+final movieService = MovieService();
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TP4 - Liste de films',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        useMaterial3: true,
+      ),
+      home: MovieListPage(movieService: movieService),
+    );
+  }
+}
